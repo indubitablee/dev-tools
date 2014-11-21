6d9063f607d7e3ae12aedfcfb595b267852a0eea
Grace Lee <glee@definedlogic.com>
2014-11-21 08:50:19 -0500

    cleaning up sticky-footer

--- sticky-footer.php
+++ sticky-footer.php
@@ -7,9 +7,6 @@
 	<script src="https://code.jquery.com/jquery-1.11.1.js"></script>
 	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 	<style>
-		* {
-			margin:0;
-		}
 		html, body {
 			height: 100%;
 		}
