41a7c0f33afc8d6c085295a1b846ef0a036f258a
Grace Lee <glee@definedlogic.com>
2014-11-21 16:15:36 -0500

    centering a div in the center of a page vertically and horizontally

---
+++ page-center.php
@@ -0,0 +1,30 @@
+<html>
+<header>
+	<title>Page Center</title>
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.2.0/css/font-awesome.min.css" />
+
+	<script src="https://code.jquery.com/jquery-1.11.1.js"></script>
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+
+	<style>
+		.center {
+			width: 100px;
+			height: 100px;
+			background-color: red;
+
+			position: absolute;
+			top: 0;
+			bottom: 0;
+			left: 0;
+			right: 0;
+			margin: auto;
+		}
+	</style>
+</header>
+<body>
+	<div class="center">
+		<!-- your stuff here -->
+	</div>	
+</body>
+</html>
