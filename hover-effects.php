b30c98bf367811410cfa89c545863e1e753a6078
Grace Lee <glee@definedlogic.com>
2014-11-28 15:33:36 -0500

    added cool hover effects that are on a website

---
+++ hover-effects.php
@@ -0,0 +1,175 @@
+<html>
+<header>
+	<title>Hover Effects</title>
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.2.0/css/font-awesome.min.css" />
+	<link rel="stylesheet" href="css/hover-effects.css" />
+
+	<script src="https://code.jquery.com/jquery-1.11.1.js"></script>
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+
+</header>
+<body>
+	<div class="col-md-12" style="margin-bottom:50px; margin-top:50px;">
+		<div class="text-center">
+			<div class="row">
+				<div class="col-md-6">
+					There are more hover effects in the css, but these are some of my favorites<br/>
+					The full original set of hover effects can be found here: <br/>
+					<a href="http://tympanus.net/Development/HoverEffectIdeas/index.html">
+						<strong>http://tympanus.net/Development/HoverEffectIdeas/index.html</strong>
+					</a>
+				</div>
+				<div class="col-md-6 text-left">
+					<img src="img/beach.jpg" />
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-4">
+				<div class="grid">
+					<figure class="effect-layla">
+						<img src="img/beach.jpg">
+						<figcaption>
+							<h2>Crazy <span>Layla</span></h2>
+							<p>When Layla appears, she brings an eternal summer along.</p>
+							<a href="#">View more</a>
+						</figcaption>			
+					</figure>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="grid">
+					<figure class="effect-oscar">
+						<img src="img/beach.jpg">
+						<figcaption>
+							<h2>Warm <span>Oscar</span></h2>
+							<p>Oscar is a decent man. He used to clean porches with pleasure.</p>
+							<a href="#">View more</a>
+						</figcaption>			
+					</figure>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="grid">
+					<figure class="effect-marley">
+						<img src="img/beach.jpg">
+						<figcaption>
+							<h2>Sweet <span>Marley</span></h2>
+							<p>Marley tried to convince her but she was not interested.</p>
+							<a href="#">View more</a>
+						</figcaption>			
+					</figure>
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-4">
+				<div class="grid">
+					<figure class="effect-ruby">
+						<img src="img/beach.jpg">
+						<figcaption>
+							<h2>Glowing <span>Ruby</span></h2>
+							<p>Ruby did not need any help. Everybody knew that.</p>
+							<a href="#">View more</a>
+						</figcaption>			
+					</figure>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="grid">
+					<figure class="effect-roxy">
+						<img src="img/beach.jpg">
+						<figcaption>
+							<h2>Charming <span>Roxy</span></h2>
+							<p>Roxy was my best friend. She'd cross any border for me.</p>
+							<a href="#">View more</a>
+						</figcaption>			
+					</figure>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="grid">
+					<figure class="effect-bubba">
+						<img src="img/beach.jpg">
+						<figcaption>
+							<h2>Fresh <span>Bubba</span></h2>
+							<p>Bubba likes to appear out of thin air.</p>
+							<a href="#">View more</a>
+						</figcaption>			
+					</figure>
+				</div>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-md-4">
+				<div class="grid">
+					<figure class="effect-sarah">
+						<img src="img/beach.jpg">
+						<figcaption>
+							<h2>Free <span>Sarah</span></h2>
+							<p>Sarah likes to watch clouds. She's quite depressed.</p>
+							<a href="#">View more</a>
+						</figcaption>			
+					</figure>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="grid">
+					<figure class="effect-chico">
+						<img src="img/beach.jpg">
+						<figcaption>
+							<h2>Silly <span>Chico</span></h2>
+							<p>Chico's main fear was missing the morning bus.</p>
+							<a href="#">View more</a>
+						</figcaption>			
+					</figure>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="grid">
+					<figure class="effect-steve">
+						<img src="img/beach.jpg">
+						<figcaption>
+							<h2>Lonely <span>Steve</span></h2>
+							<p>Steve was afraid of the Boogieman.</p>
+							<a href="#">View more</a>
+						</figcaption>			
+					</figure>
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-4">
+				<div class="grid">
+					<figure class="effect-ming">
+						<img src="img/beach.jpg">
+						<figcaption>
+							<h2>Funny <span>Ming</span></h2>
+							<p>Ming sits in the corner the whole day. She's into crochet.</p>
+							<a href="#">View more</a>
+						</figcaption>			
+					</figure>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="grid">
+					
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="grid">
+					
+				</div>
+			</div>
+		</div>
+		
+		
+	</div>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<header>
+	<title>Page Center</title>
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.2.0/css/font-awesome.min.css" />
+	<link rel="stylesheet" href="css/hover-effects.css" />
+
+	<script src="https://code.jquery.com/jquery-1.11.1.js"></script>
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+
+</header>
+<body>
+	<a href="hover-effects.php">hover effects</a>
+</body>
+</html>
