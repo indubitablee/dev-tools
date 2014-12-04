936f056e0d72adc3b5b1a9d78dbbcd7656372375
Grace Lee <glee@definedlogic.com>
2014-12-04 08:50:19 -0500

    added some css to radio buttons and checkbox rework, check later

--- radio-checkbox-css.php
+++ radio-checkbox-css.php
@@ -55,6 +55,87 @@
 			background-image: url('img/checked.jpg');
 			background-repeat: no-repeat;
 		}
+
+
+/*****CUSTOM INPUT CSS******/
+.blue-textbox {
+	background-color: #D5E1ED;
+	font-size: 17px;
+	font-weight: bold;
+	border-radius: 0px;
+	border: none;
+	/*webkit-box-shadow: none;*/
+	box-shadow: none;
+}
+.blue-textbox:focus {
+	border: none;
+	/*webkit-box-shadow: none;*/
+	box-shadow: none;
+}
+
+/*hide checkbox and radio buttons*/
+input[type=checkbox].blue-input, 
+input[type=radio].blue-input {
+	width: 35px;
+	height: 35px;
+	margin: 0;
+	padding: 0;
+	font-size: 1em;
+	opacity: 0; /*This is the part tht actually hides it*/
+	z-index: 1;
+}
+/*This makes the input overlap the image*/
+input[type=checkbox].blue-input {
+	margin-left: -35px;
+	margin-right: 0px;
+}
+.checkbox-label {
+	width: 35px;
+	height: 35px;
+	margin-bottom: 0;
+}
+input[type=radio].blue-input {
+	margin-left: 0px !important;
+}
+/*normalize the spacing*/
+input[type=checkbox].blue-input + label,
+input[type=radio].blue-input + label {
+	display: inline-block;
+	margin-left: -20px;
+	line-height: 1.5em;
+}
+/*unchecked css*/
+input[type=checkbox].blue-input + label > span,
+input[type=radio].blue-input + label > span {
+	display: inline-block;
+	background-color: #D5E1ED;
+	width: 35px;
+	height: 35px;
+	border-radius: 30px;
+}
+input[type=radio].blue-input + label > span {
+	margin-right: 10px;
+}
+.blue-input:checked + label > span > span {
+	display: block;
+	width: 35px;
+	height: 35px;
+	background-image: url('/content/img/checked.jpg');
+	background-repeat: no-repeat;
+}
+.radio-check-text {
+	display: inline;
+	position: absolute;
+	margin-top: 7px;
+	z-index: -1;
+}
+input[type=checkbox].blue-input:checked .radio-check-text{
+	font-weight: bold;
+}
+input[type=checkbox].blue-input:hover , 
+input[type=radio].blue-input:hover {
+	cursor: pointer;
+}
 	</style>
 </header>
 <body>
